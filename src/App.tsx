f17daeb0ef4f27de521c2dd2234b60ad792168f2
Hyunkuk Kim <60034879+rtscts0987@users.noreply.github.com>
2023-02-21 22:44:14 +0900

    0.3.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import Circle from './Circle';
+import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // console.log(event.currentTarget.value);
+    // setValue("Hello");
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello",value);
+  };
   return (
     <div>
-      <Circle borderColor='yellow' bgColor='blue'/>
-      <Circle bgColor='tomato'/>
+      <form onSubmit={onSubmit}>
+        <input value={value} type="text" onChange={onChange} placeholder="username"/>
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
