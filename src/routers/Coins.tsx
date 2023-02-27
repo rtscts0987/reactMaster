520b9e88b9b2805bc266d1fa67d021ef8728d5d5
Hyunkuk Kim <60034879+rtscts0987@users.noreply.github.com>
2023-02-27 21:22:50 +0900

    0.5.3

--- src/routers/Coins.tsx
+++ src/routers/Coins.tsx
@@ -1,5 +1,8 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import {useState,useEffect} from 'react';
+import imsi from './Imsi'
+
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -37,49 +40,57 @@ const Title = styled.h1`
     color:${(props) => props.theme.accentColor};
 `;
 
-const coins = [{
-    "id": "btc-bitcoin",
-    "name": "Bitcoin",
-    "symbol": "BTC",
-    "rank": 1,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin"
-  },
-  {
-    "id": "eth-ethereum",
-    "name": "Ethereum",
-    "symbol": "ETH",
-    "rank": 2,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin"
-  },
-  {
-    "id": "usdt-tether",
-    "name": "Tether",
-    "symbol": "USDT",
-    "rank": 3,
-    "is_new": false,
-    "is_active": true,
-    "type": "token"
-  },
-]
+const Lodder = styled.span`
+    color: red;
+    font-size:100px;
+    text-align: center;
 
-const Coins = () => {
+`;
+
+interface CoinInterface {
+    "id": string,
+    "name": string,
+    "symbol": string,
+    "rank": number,
+    "is_new": boolean,
+    "is_active": boolean,
+    "type": string
+}
+
+function Coins() {
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loding, setLoding] = useState(true);
+    useEffect( () => {
+        //imsi use
+        // (async() => {
+        //     const response = await fetch("https://api.coinpaprika.com/v1/coins");
+        //     let json = await response.json();
+        //     
+        //     json = imsi;
+        //     setCoins(json.slice(0,100));
+        //     setLoding(false);
+        // })();
+        const json = imsi;
+        setCoins(json.slice(0,100));
+        setLoding(false);
+    },[])
+    
     return (
         <>
         <Container>
             <Header>
                 <Title>Coins</Title>
             </Header>
-            <CoinsList>
-                {coins.map(coin => 
-                    <Coin key={coin.id}>
-                        <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link> 
-                    </Coin> )}
-                
-            </CoinsList>
+            {loding ? <Lodder>Lodding....</Lodder> : (
+                <CoinsList>
+                    {coins.map(coin => 
+                        <Coin key={coin.id}>
+                            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link> 
+                        </Coin> )}
+                    
+                </CoinsList>
+                )
+            }
         </Container>
         </>
     );
